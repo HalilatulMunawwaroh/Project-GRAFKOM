0e39c88e9f9d8eb51da76e92424c994285326e9d
Dieva06 <71698611+Dieva06@users.noreply.github.com>
2020-12-01 22:12:04 +0700

    gerakin object

---
+++ Tank.cpp
@@ -0,0 +1,233 @@
+#include <windows.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+
+
+int angle;
+float horizontalMove;
+float verticalMove;
+
+// Collider
+float posisiX[2] = {0, 15};
+float posisiY[2] = {0, 10};
+
+
+void ColliderPersegi(){ // Collider bentuk kotak
+    glPushMatrix();
+    glBegin(GL_LINES);
+        glVertex2f(posisiX[0], posisiY[0]);
+        glVertex2f(posisiX[1], posisiY[0]);
+        glVertex2f(posisiX[1], posisiY[1]);
+        glVertex2f(posisiX[0], posisiY[1]);
+        glEnd();
+    glPopMatrix();
+}
+
+
+void TankView(){
+    //atas
+    glMatrixMode(GL_MODELVIEW);
+    glColor3f(1.0,0.0,1.0);
+    glBegin(GL_POLYGON);
+        glVertex2f(16.71, 9);
+        glVertex2f(16.67, 10.2);
+        glVertex2f(16.02, 10.26);
+        glVertex2f(15.45, 10.73);
+        glVertex2f(15.45, 11.23);
+        glVertex2f(15.71, 11.59);
+        glVertex2f(16.28, 11.82);
+        glVertex2f(16.93, 11.82);
+        glVertex2f(17.57, 11.84);
+        glVertex2f(18.15, 11.78);
+        glVertex2f(18.66, 11.53);
+        glVertex2f(18.75, 10.99);
+        glVertex2f(18.43, 10.5);
+        glVertex2f(18.08, 10.28);
+        glVertex2f(17.49, 10.28);
+        glVertex2f(17.49, 9);
+
+    glEnd();
+
+    glMatrixMode(GL_MODELVIEW);
+    glColor3f(1,0.5,0);
+    glBegin(GL_POLYGON);
+        glVertex2f(16, 9);
+        glVertex2f(18.23, 9);
+        glVertex2f(19.26, 7.25);
+        glVertex2f(14.82, 7.21);
+
+    glEnd();
+
+	//body
+    glMatrixMode(GL_MODELVIEW);
+    glColor3f(0.5,0,0);
+    glBegin(GL_POLYGON);
+        glVertex2f(13.97, 7.21);
+        glVertex2f(19.95, 7.24);
+        glVertex2f(22, 4);
+        glVertex2f(12, 4);
+
+	glEnd();
+
+
+//rodaBelakang
+    glMatrixMode(GL_MODELVIEW);
+    glColor3f(0.0,0.0,0.0);
+    glBegin(GL_POLYGON);
+        glVertex2f(14.57, 4.8);
+        glVertex2f(14.29, 4.63);
+        glVertex2f(14.1, 4.26);
+        glVertex2f(14.17, 4);
+        glVertex2f(14.26, 3.71);
+        glVertex2f(14.5, 3.5);
+        glVertex2f(15, 3.5);
+        glVertex2f(15.26, 3.57);
+        glVertex2f(15.5, 3.75);
+        glVertex2f(15.65, 4.01);
+        glVertex2f(15.6, 4.35);
+        glVertex2f(15.45, 4.66);
+        glVertex2f(15.09, 4.82);
+
+    glEnd();
+
+
+//rodaDepan
+    glMatrixMode(GL_MODELVIEW);
+    glColor3f(0.0,0.0,0.0);
+    glBegin(GL_POLYGON);
+        glVertex2f(18.68, 4.75);
+        glVertex2f(18.52, 4.58);
+        glVertex2f(18.4, 4.4);
+        glVertex2f(18.36, 4.19);
+        glVertex2f(18.4, 4);
+        glVertex2f(18.51, 3.84);
+        glVertex2f(18.66, 3.64);
+        glVertex2f(19.01, 3.51);
+        glVertex2f(19.37, 3.51);
+        glVertex2f(19.59, 3.62);
+        glVertex2f(19.82, 3.84);
+        glVertex2f(19.88, 4.02);
+        glVertex2f(19.94, 4.36);
+        glVertex2f(19.85, 4.55);
+        glVertex2f(19.65, 4.68);
+        glVertex2f(19.43, 4.8);
+        glVertex2f(19.06, 4.84);
+    glEnd();
+
+//belakang
+    glMatrixMode(GL_MODELVIEW);
+    glColor3f(0,0,0.5);
+    glBegin(GL_POLYGON);
+        glVertex2f(13.23, 6.02);
+        glVertex2f(11.73, 6.57);
+        glVertex2f(11.64, 5.47);
+        glVertex2f(12.72, 5.18);
+    glEnd();
+
+    //tembak
+    glMatrixMode(GL_MODELVIEW);
+    glColor3f(0,0,0.5);
+    glBegin(GL_POLYGON);
+        glVertex2f(20.74, 6);
+        glVertex2f(25, 6);
+        glVertex2f(25, 5.3);
+        glVertex2f(21.16, 5.33);
+    glEnd();
+
+}
+
+
+void TankObject(){
+    // Memasang collider pada object
+    ColliderPersegi();
+
+    glPushMatrix();
+    glTranslatef(horizontalMove, verticalMove,0); // Mengendalikan posisi tank
+    glTranslatef(-12,-3,0); // Menengahkan view
+    TankView(); // Memanggil view
+    glPopMatrix();
+}
+
+// Fungsi untuk mengendalikan object
+void characterController(int data)
+{
+    // Jika menekan tombol panah kiri
+    if(GetAsyncKeyState(VK_LEFT)){
+        if (posisiX[0] >= 10){
+            posisiX[0] -= 0.1f;
+            posisiX[1] -= 0.1f;
+            horizontalMove-=0.1f;
+        }
+    // Jika menekan tombol panah kanan
+    } else if(GetAsyncKeyState(VK_RIGHT)){
+        if (posisiX[1] <= 60){
+            posisiX[0] += 0.1f;
+            posisiX[1] += 0.1f;
+            horizontalMove+=0.1f;
+        }
+    }
+
+    // Jika menekan tombol panah atas
+    if(GetAsyncKeyState(VK_UP)){
+        if (posisiY[1] <= 15){
+            posisiY[0] += 0.1f;
+            posisiY[1] += 0.1f;
+            verticalMove+=0.1f;
+        }
+    // Jika menekan tombol panah bawah
+    } else if (GetAsyncKeyState(VK_DOWN)){
+        if (posisiY[0] >= -5){
+            posisiY[0] -= 0.1f;
+            posisiY[1] -= 0.1f;
+            verticalMove-=0.1f;
+        }
+    }
+
+    glutPostRedisplay();
+	glutTimerFunc(1,characterController,0);
+}
+
+void displayMe(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+    glColor3ub(52, 225,235);
+    glBegin(GL_QUADS);
+        glVertex2d(-900, 900);
+        glVertex2d(900, 900);
+        glVertex2d(900, -900);
+        glVertex2d(-900, -900);
+    glEnd();
+
+
+	// Memanggil object tank
+    TankObject();
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+void myinit() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glClearColor(1.0, 1.0, 1.0, 1.0); //membersihkan windows
+	glColor3f(0.0, 0.0, 0.0); //spesifikasi warna
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(900, 900);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Windows");
+	gluOrtho2D(10, 60, -5, 30);
+	glutDisplayFunc(displayMe);
+	glutTimerFunc(1,characterController,0);
+
+	myinit();
+	glutMainLoop();
+	return 0;
+}
