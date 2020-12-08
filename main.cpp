aa1d16bb291c94e542bc7abde0845a102809f935
HalilatulMunawwaroh <71701721+HalilatulMunawwaroh@users.noreply.github.com>
2020-12-08 18:23:37 +0700

    memperbarui awan dan jalan

--- main.cpp
+++ main.cpp
@@ -2,10 +2,25 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-void displayMe(void){
-//baground
-    glClear(GL_COLOR_BUFFER_BIT);
+float x_pos1 = 0.0;
+float x_pos2 = 0.0;
+float x_pos3 = 0.0;
+float x_pos4 = 0.0;
+float x_pos5 = 0.0;
+float x_pos6 = 0.0;
+float delta = 0.015;
+float jalan_pos1 = 0.0;
+float jalan_pos2 = 0.0;
+float jalan_pos3 = 0.0;
+float jalan_pos4 = 0.0;
+float jalan_pos5 = 0.0;
+float jalan_pos6 = 0.0;
+float delta2 = 0.1;
+bool toLeft = true;
 
+
+void baground(){
+    glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(52, 225,235);
     glBegin(GL_QUADS);
@@ -14,226 +29,139 @@ void displayMe(void){
         glVertex2d(900, -900);
         glVertex2d(-900, -900);
     glEnd();
-    glFlush();
+}
 
+void awan(){
 //awan1
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(12, 23);
-        glVertex2f(12.25, 23.57);
-        glVertex2f(12.56, 23.9);
-        glVertex2f(12.9, 24.1);
-        glVertex2f(13.2, 24.51);
-        glVertex2f(13.44, 24.86);
-        glVertex2f(14, 25);
-        glVertex2f(14.56, 24.86);
-        glVertex2f(14.87, 24.51);
-        glVertex2f(15, 24);
-        glVertex2f(15.42, 23.87);
-        glVertex2f(15.78, 23.56);
-        glVertex2f(16, 23);
+        glVertex2f(12 + x_pos1, 23);
+        glVertex2f(12.25 + x_pos1, 23.57);
+        glVertex2f(12.56 + x_pos1, 23.9);
+        glVertex2f(12.9 + x_pos1, 24.1);
+        glVertex2f(13.2 + x_pos1, 24.51);
+        glVertex2f(13.44 + x_pos1, 24.86);
+        glVertex2f(14 + x_pos1, 25);
+        glVertex2f(14.56 + x_pos1, 24.86);
+        glVertex2f(14.87 + x_pos1, 24.51);
+        glVertex2f(15 + x_pos1, 24);
+        glVertex2f(15.42 + x_pos1, 23.87);
+        glVertex2f(15.78 + x_pos1, 23.56);
+        glVertex2f(16 + x_pos1, 23);
     glEnd();
-    glFlush();
 
 //awan2
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(16.8, 20);
-        glVertex2f(16.58, 20.4);
-        glVertex2f(16.73, 20.85);
-        glVertex2f(17.14, 21.04);
-        glVertex2f(17.56, 20.9);
-        glVertex2f(17.69, 20.58);
-        glVertex2f(18, 20.74);
-        glVertex2f(18.34, 20.66);
-        glVertex2f(18.55, 20.4);
-        glVertex2f(18.42, 20);
+        glVertex2f(16.8 + x_pos2, 20);
+        glVertex2f(16.58 + x_pos2, 20.4);
+        glVertex2f(16.73 + x_pos2, 20.85);
+        glVertex2f(17.14 + x_pos2, 21.04);
+        glVertex2f(17.56 + x_pos2, 20.9);
+        glVertex2f(17.69 + x_pos2, 20.58);
+        glVertex2f(18 + x_pos2, 20.74);
+        glVertex2f(18.34 + x_pos2, 20.66);
+        glVertex2f(18.55 + x_pos2, 20.4);
+        glVertex2f(18.42 + x_pos2, 20);
     glEnd();
-    glFlush();
 
 //awan3
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(22, 24);
-        glVertex2f(21.27, 24.3);
-        glVertex2f(21, 25);
-        glVertex2f(21.37, 25.77);
-        glVertex2f(22, 26);
-        glVertex2f(22.2, 26.82);
-        glVertex2f(22.71, 27.61);
-        glVertex2f(23.29, 28);
-        glVertex2f(24, 28.2);
-        glVertex2f(24.7, 28);
-        glVertex2f(25.35, 27.54);
-        glVertex2f(25.66, 26.91);
-        glVertex2f(26.26, 27.1);
-        glVertex2f(27, 27);
-        glVertex2f(27.52, 26.45);
-        glVertex2f(27.6, 25.86);
-        glVertex2f(27.3, 25.23);
-        glVertex2f(27.75, 25.5);
-        glVertex2f(28.3, 25.4);
-        glVertex2f(28.62, 25);
-        glVertex2f(28.5, 24.4);
-        glVertex2f(28, 24);
+        glVertex2f(22 + x_pos3, 24);
+        glVertex2f(21.27 + x_pos3, 24.3);
+        glVertex2f(21 + x_pos3, 25);
+        glVertex2f(21.37 + x_pos3, 25.77);
+        glVertex2f(22 + x_pos3, 26);
+        glVertex2f(22.2 + x_pos3, 26.82);
+        glVertex2f(22.71 + x_pos3, 27.61);
+        glVertex2f(23.29 + x_pos3, 28);
+        glVertex2f(24 + x_pos3, 28.2);
+        glVertex2f(24.7 + x_pos3, 28);
+        glVertex2f(25.35 + x_pos3, 27.54);
+        glVertex2f(25.66 + x_pos3, 26.91);
+        glVertex2f(26.26 + x_pos3, 27.1);
+        glVertex2f(27 + x_pos3, 27);
+        glVertex2f(27.52 + x_pos3, 26.45);
+        glVertex2f(27.6 + x_pos3, 25.86);
+        glVertex2f(27.3 + x_pos3, 25.23);
+        glVertex2f(27.75 + x_pos3, 25.5);
+        glVertex2f(28.3 + x_pos3, 25.4);
+        glVertex2f(28.62 + x_pos3, 25);
+        glVertex2f(28.5 + x_pos3, 24.4);
+        glVertex2f(28 + x_pos3, 24);
     glEnd();
-    glFlush();
 
 //awan4
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(34, 22);
-        glVertex2f(33.54, 22.27);
-        glVertex2f(33.59, 22.81);
-        glVertex2f(34, 23);
-        glVertex2f(34.38, 22.76);
-        glVertex2f(34.25, 23.32);
-        glVertex2f(34.57, 23.83);
-        glVertex2f(35.23, 24.14);
-        glVertex2f(36, 24.24);
-        glVertex2f(36.88, 24.14);
-        glVertex2f(37.45, 23.81);
-        glVertex2f(37.76, 23.36);
-        glVertex2f(37.64, 22.78);
-        glVertex2f(38, 23);
-        glVertex2f(38.42, 22.85);
-        glVertex2f(38.5, 22.27);
-        glVertex2f(38, 22);
+        glVertex2f(34 + x_pos4, 22);
+        glVertex2f(33.54 + x_pos4, 22.27);
+        glVertex2f(33.59 + x_pos4, 22.81);
+        glVertex2f(34 + x_pos4, 23);
+        glVertex2f(34.38 + x_pos4, 22.76);
+        glVertex2f(34.25 + x_pos4, 23.32);
+        glVertex2f(34.57 + x_pos4, 23.83);
+        glVertex2f(35.23 + x_pos4, 24.14);
+        glVertex2f(36 + x_pos4, 24.24);
+        glVertex2f(36.88 + x_pos4, 24.14);
+        glVertex2f(37.45 + x_pos4, 23.81);
+        glVertex2f(37.76 + x_pos4, 23.36);
+        glVertex2f(37.64 + x_pos4, 22.78);
+        glVertex2f(38 + x_pos4, 23);
+        glVertex2f(38.42 + x_pos4, 22.85);
+        glVertex2f(38.5 + x_pos4, 22.27);
+        glVertex2f(38 + x_pos4, 22);
     glEnd();
-    glFlush();
 
 //awan5
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(42, 25);
-        glVertex2f(41.54, 25.42);
-        glVertex2f(41.5, 26.1);
-        glVertex2f(41.73, 26.75);
-        glVertex2f(42.29, 27.18);
-        glVertex2f(42.83, 27.25);
-        glVertex2f(43.4, 27.15);
-        glVertex2f(43.9, 26.75);
-        glVertex2f(44, 26.2);
-        glVertex2f(44, 25.7);
-        glVertex2f(44.4, 25.8);
-        glVertex2f(44.82, 25.62);
-        glVertex2f(44.84, 25.25);
-        glVertex2f(44.61, 25);
+        glVertex2f(42 + x_pos5, 25);
+        glVertex2f(41.54 + x_pos5, 25.42);
+        glVertex2f(41.5 + x_pos5, 26.1);
+        glVertex2f(41.73 + x_pos5, 26.75);
+        glVertex2f(42.29 + x_pos5, 27.18);
+        glVertex2f(42.83 + x_pos5, 27.25);
+        glVertex2f(43.4 + x_pos5, 27.15);
+        glVertex2f(43.9 + x_pos5, 26.75);
+        glVertex2f(44 + x_pos5, 26.2);
+        glVertex2f(44 + x_pos5, 25.7);
+        glVertex2f(44.4 + x_pos5, 25.8);
+        glVertex2f(44.82 + x_pos5, 25.62);
+        glVertex2f(44.84 + x_pos5, 25.25);
+        glVertex2f(44.61 + x_pos5, 25);
     glEnd();
-    glFlush();
 
 //awan6
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(50, 22);
-        glVertex2f(49, 22.47);
-        glVertex2f(48.95, 23.5);
-        glVertex2f(49.6, 24.22);
-        glVertex2f(50.47, 24.22);
-        glVertex2f(50.83, 25.18);
-        glVertex2f(51.32, 26);
-        glVertex2f(52.2, 26.4);
-        glVertex2f(53, 26.43);
-        glVertex2f(53.88, 26.35);
-        glVertex2f(54.5, 26.1);
-        glVertex2f(55, 25.56);
-        glVertex2f(55.5, 24.83);
-        glVertex2f(55.66, 24);
-        glVertex2f(56.3, 23.5);
-        glVertex2f(56.34, 22.72);
-        glVertex2f(56, 22);
+        glVertex2f(50 + x_pos6, 22);
+        glVertex2f(49 + x_pos6, 22.47);
+        glVertex2f(48.95 + x_pos6, 23.5);
+        glVertex2f(49.6 + x_pos6, 24.22);
+        glVertex2f(50.47 + x_pos6, 24.22);
+        glVertex2f(50.83 + x_pos6, 25.18);
+        glVertex2f(51.32 + x_pos6, 26);
+        glVertex2f(52.2 + x_pos6, 26.4);
+        glVertex2f(53 + x_pos6, 26.43);
+        glVertex2f(53.88 + x_pos6, 26.35);
+        glVertex2f(54.5 + x_pos6, 26.1);
+        glVertex2f(55 + x_pos6, 25.56);
+        glVertex2f(55.5 + x_pos6, 24.83);
+        glVertex2f(55.66 + x_pos6, 24);
+        glVertex2f(56.3 + x_pos6, 23.5);
+        glVertex2f(56.34 + x_pos6, 22.72);
+        glVertex2f(56 + x_pos6, 22);
     glEnd();
-    glFlush();
-	
-	//gedung
-
-    glMatrixMode(GL_MODELVIEW);
-    glColor3f(0,0,0);
-    glBegin(GL_POLYGON);
-        glVertex2f(10, 5);
-        glVertex2f(10, 18);
-        glVertex2f(12, 18);
-        glVertex2f(12, 17);
-        glVertex2f(13, 17);
-        glVertex2f(12.99, 19.24);
-        glVertex2f(22, 19.24);
-        glVertex2f(22, 14);
-        glVertex2f(30, 14);
-        glVertex2f(29.97, 20.84);
-        glVertex2f(38.02, 20.95);
-        glVertex2f(38.02, 11.33);
-        glVertex2f(41.96, 11.3);
-        glVertex2f(42, 18);
-        glVertex2f(48, 18);
-        glVertex2f(47.99, 20.79);
-        glVertex2f(59.99, 20.74);
-        glVertex2f(60, 5);
-
-    glEnd();
-    glFlush();
-
-//jendela
-
-    glMatrixMode(GL_MODELVIEW);
-    glColor3ub(237, 236, 173);
-    glBegin(GL_QUADS);
-        glVertex2f(32.95, 18.61);
-        glVertex2f(36, 18.61);
-        glVertex2f(36, 16);
-        glVertex2f(32.95, 16);
-
-    glEnd();
-    glFlush();
-
-    glMatrixMode(GL_MODELVIEW);
-    glColor3ub(237, 236, 173);
-    glBegin(GL_QUADS);
-        glVertex2f(45, 13.12);
-        glVertex2f(48, 13.12);
-        glVertex2f(48,10);
-        glVertex2f(45, 10);
-
-    glEnd();
-    glFlush();
-
-    glMatrixMode(GL_MODELVIEW);
-    glColor3ub(237, 236, 173);
-    glBegin(GL_QUADS);
-        glVertex2f(53.11, 18.77);
-        glVertex2f(56, 18.77);
-        glVertex2f(56, 15.67);
-        glVertex2f(53.11, 15.67);
-
-    glEnd();
-    glFlush();
-
-    glMatrixMode(GL_MODELVIEW);
-    glColor3ub(237, 236, 173);
-    glBegin(GL_QUADS);
-        glVertex2f(16, 18);
-        glVertex2f(18, 18);
-        glVertex2f(18, 16);
-        glVertex2f(16, 16);
-
-    glEnd();
-    glFlush();
-
-    glMatrixMode(GL_MODELVIEW);
-    glColor3ub(237, 236, 173);
-    glBegin(GL_QUADS);
-        glVertex2f(24, 13.15);
-        glVertex2f(26, 13.15);
-        glVertex2f(26, 11.23);
-        glVertex2f(24, 11.23);
-
-    glEnd();
-    glFlush();
+}
+void jalan(){
 
 //jalan
     glMatrixMode(GL_MODELVIEW);
@@ -244,69 +172,188 @@ void displayMe(void){
         glVertex2f(60, -5);
         glVertex2f(60, 5);
     glEnd();
-    glFlush();
 
 //garis jalan
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(12, 1);
-        glVertex2f(16, 1);
-        glVertex2f(16, -1);
-        glVertex2f(12, -1);
+        glVertex2f(12 + jalan_pos1, 1);
+        glVertex2f(16 + jalan_pos1, 1);
+        glVertex2f(16 + jalan_pos1, -1);
+        glVertex2f(12 + jalan_pos1, -1);
     glEnd();
-    glFlush();
 
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(20, 1);
-        glVertex2f(24, 1);
-        glVertex2f(24, -1);
-        glVertex2f(20, -1);
+        glVertex2f(20 + jalan_pos2, 1);
+        glVertex2f(24 + jalan_pos2, 1);
+        glVertex2f(24 + jalan_pos2, -1);
+        glVertex2f(20 + jalan_pos2, -1);
     glEnd();
-    glFlush();
 
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(28, 1);
-        glVertex2f(32, 1);
-        glVertex2f(32, -1);
-        glVertex2f(28, -1);
+        glVertex2f(28 + jalan_pos3, 1);
+        glVertex2f(32 + jalan_pos3, 1);
+        glVertex2f(32 + jalan_pos3, -1);
+        glVertex2f(28 + jalan_pos3, -1);
     glEnd();
-    glFlush();
 
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(36, 1);
-        glVertex2f(40, 1);
-        glVertex2f(40, -1);
-        glVertex2f(36, -1);
+        glVertex2f(36 + jalan_pos4, 1);
+        glVertex2f(40 + jalan_pos4, 1);
+        glVertex2f(40 + jalan_pos4, -1);
+        glVertex2f(36 + jalan_pos4, -1);
     glEnd();
-    glFlush();
 
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(44, 1);
-        glVertex2f(48, 1);
-        glVertex2f(48, -1);
-        glVertex2f(44, -1);
+        glVertex2f(44 + jalan_pos5, 1);
+        glVertex2f(48 + jalan_pos5, 1);
+        glVertex2f(48 + jalan_pos5, -1);
+        glVertex2f(44 + jalan_pos5, -1);
     glEnd();
-    glFlush();
 
     glMatrixMode(GL_MODELVIEW);
     glColor3ub(252, 252, 252);
     glBegin(GL_POLYGON);
-        glVertex2f(52, 1);
-        glVertex2f(56, 1);
-        glVertex2f(56, -1);
-        glVertex2f(52, -1);
+        glVertex2f(52 + jalan_pos6, 1);
+        glVertex2f(56 + jalan_pos6, 1);
+        glVertex2f(56 + jalan_pos6, -1);
+        glVertex2f(52 + jalan_pos6, -1);
     glEnd();
-    glFlush();
-	
+}
+
+void timer(int){
+    glutPostRedisplay();
+    glutTimerFunc(1000/30, timer, 0);
+    if(x_pos1<50 && toLeft)
+        if(x_pos1 < -7){
+            x_pos1 = 45;
+        }
+        else{
+            x_pos1 -= delta;
+        }
+    else
+        toLeft = true;
+
+    if(x_pos2<50 && toLeft)
+        if(x_pos2 < -10){
+            x_pos2 = 42;
+        }
+        else{
+            x_pos2 -= delta;
+        }
+    else
+        toLeft = true;
+
+    if(x_pos3<50 && toLeft)
+        if(x_pos3 < -20){
+            x_pos3 = 38;
+        }
+        else{
+            x_pos3 -= delta;
+        }
+    else
+        toLeft = true;
+
+    if(x_pos4<50 && toLeft)
+        if(x_pos4 < -30){
+            x_pos4 = 30;
+        }
+        else{
+            x_pos4 -= delta;
+        }
+    else
+        toLeft = true;
+
+    if(x_pos5<50 && toLeft)
+        if(x_pos5 < -35){
+            x_pos5 = 25;
+        }
+        else{
+            x_pos5 -= delta;
+        }
+    else
+        toLeft = true;
+
+    if(x_pos6<50 && toLeft)
+        if(x_pos6 < -50){
+            x_pos6 = 15;
+        }
+        else{
+            x_pos6 -= delta;
+        }
+    else
+        toLeft = true;
+
+
+    if(jalan_pos1<50 && toLeft)
+        if(jalan_pos1 < -6){
+            jalan_pos1 = 42;
+        }
+        else{
+            jalan_pos1 -= delta2;
+        }
+    else
+        toLeft = true;
+
+    if(jalan_pos2<50 && toLeft)
+        if(jalan_pos2 < -14){
+            jalan_pos2 = 34;
+        }
+        else{
+            jalan_pos2 -= delta2;
+        }
+    else
+        toLeft = true;
+
+    if(jalan_pos3<50 && toLeft)
+        if(jalan_pos3 < -22){
+            jalan_pos3 = 26;
+        }
+        else{
+            jalan_pos3 -= delta2;
+        }
+    else
+        toLeft = true;
+
+    if(jalan_pos4<50 && toLeft)
+        if(jalan_pos4 < -30){
+            jalan_pos4 = 18;
+        }
+        else{
+            jalan_pos4 -= delta2;
+        }
+    else
+        toLeft = true;
+
+    if(jalan_pos5<50 && toLeft)
+        if(jalan_pos5 < -38){
+            jalan_pos5 = 10;
+        }
+        else{
+            jalan_pos5 -= delta2;
+        }
+    else
+        toLeft = true;
+
+    if(jalan_pos6<50 && toLeft)
+        if(jalan_pos6 < -46){
+            jalan_pos6 = 2;
+        }
+        else{
+            jalan_pos6 -= delta2;
+        }
+    else
+        toLeft = true;
+}
+
 //Gambar karakter tank
 //atas
     glMatrixMode(GL_MODELVIEW);
@@ -555,13 +602,27 @@ void displayMe(void){
 	glFlush();
 }
 
+void displayMe(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+    baground();
+    awan();
+    jalan();
 
-int main(int argc, char** argv){
+	glFlush();
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
 	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(900, 900);
-	glutInitWindowPosition(0,0);
-	glutCreateWindow("Hello world!");
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Windows");
+	gluOrtho2D(10, 60, -5, 45);
 	glutDisplayFunc(displayMe);
-	gluOrtho2D(10, 60, -5, 30);
+	glutTimerFunc(0, timer, 0);
+
 	glutMainLoop();
+	return 0;
 }
